cf99bc84a931e9038a66fb0f183c4da81e9f7fa0
Rico Ardiansyah. <festyus09@gmail.com>
2025-07-07 02:52:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const Config = {
-	phone_number: process.env.PHONE_NUMBER || "62xxxx", // Number BOT for pairing
-	owners: process.env.OWNERS ? process.env.OWNERS.split(",") : ["62xxxx"], // Comma-separated list in ENV
+	phone_number: process.env.PHONE_NUMBER || "6282135214781", // Number BOT for pairing
+	owners: process.env.OWNERS ? process.env.OWNERS.split(",") : ["6283150958207"], // Comma-separated list in ENV
 
 	use_pairing_code: process.env.USE_PAIRING_CODE
 		? process.env.USE_PAIRING_CODE.toLowerCase() === "true"
@@ -13,21 +13,21 @@ export const Config = {
 		? process.env.PREFIX.split(",")
 		: ["!", ".", "+"],
 
-	maelyn_apikey: process.env.MAELYN_APIKEY || "", // https://maelyn.sbs
+	maelyn_apikey: process.env.MAELYN_APIKEY || "mg-T7Grb8DaBZpE4yGCuyWOsujFD1UH3wU2", // https://maelyn.sbs
 	bing_cookie: process.env.BING_COOKIE || "", // _U
 
 	// change the timezone to your timezone
 	timezone: process.env.TIMEZONE || "Asia/Jakarta",
 
 	profile: {
-		namebot: process.env.PROFILE_NAMEBOT || "Kurodate Haruna",
+		namebot: process.env.PROFILE_NAMEBOT || "Abbie",
 		powered: process.env.PROFILE_POWERED || "By Maelyn APIs",
 		web: process.env.PROFILE_WEB || "https://maelyn.sbs",
 	},
 
 	images: {
-		menu: process.env.IMAGES_MENU || "https://s6.imgcdn.dev/Yc8bUC.png",
-		allmenu: process.env.IMAGES_ALLMENU || "https://s6.imgcdn.dev/Yc8bUC.png",
+		menu: process.env.IMAGES_MENU || "",
+		allmenu: process.env.IMAGES_ALLMENU || "",
 	},
 
 	database: {
